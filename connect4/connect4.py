84de3845557a4b6b8ef4b69da92519c3cbc20ddd
Alberto Torralba <totoal@uv.es>
2021-06-13 20:50:00 +0200

    Removed exception() because it was a bit pointless

--- connect4/connect4.py
+++ connect4/connect4.py
@@ -11,10 +11,10 @@ elif sys.platform == 'linux':
 else:
     def cls(): os.system('clear')
 
-def exception():
-    cls()
-    if input("Error - Press q to quit or any key to continue: ") != 'q': return True
-    else: exit()
+# def exception():
+    # cls()
+    # if input("Error - Press q to quit or any key to continue: ") != 'q': return True
+    # else: exit()
 
 def printboard(state): 
     cls()
@@ -64,8 +64,7 @@ def game():
             printboard(state)
             print(playerstring[2-turn%2] + '\'s turn.')
             try: c = int(input('Where do you want to place your disc?: '))
-            except: 
-                if exception(): continue
+            except: continue
             if c > 7 or c<1: continue
             if fill(2-turn%2,state,c):
                 break
