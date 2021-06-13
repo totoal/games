163de932ee9e02af0e4493cbed9ee9ec5aae29ee
Alberto Torralba <totoal@uv.es>
2021-06-13 21:17:51 +0200

    First commit

---
+++ snake/snake.py
@@ -0,0 +1,28 @@
+############################
+''' UNDER CONSTRUCTION '''
+############################
+
+import numpy as np
+import os
+import sys
+
+if sys.platform == 'win32':
+    def cls(): os.system('cls')
+elif sys.platform == 'linux':
+    def cls(): os.system('clear')
+else:
+    def cls(): os.system('clear')
+
+def spawn_fruit(board):
+    # -1 means fruit
+    while True:
+        fruit_pos = np.random.randint(0, len(board))
+        if board[fruit_pos] == 0:
+            board[fruit_pos] = -1
+
+def init():
+    bdim = 15
+    board = np.zeros(bdim**2)
+    #Init snake
+    board[int(bdim)/2, int(bdim)/2] = 1
+    board[int(bdim)/2 + 1, int(bdim)/2] = 2
