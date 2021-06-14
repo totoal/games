c3d42c4263f0c336df0107bb5b971542de31046a
Alberto Torralba <totoal@uv.es>
2021-06-14 09:13:33 +0200

    Init functions ready

--- snake/snake.py
+++ snake/snake.py
@@ -16,13 +16,24 @@ else:
 def spawn_fruit(board):
     # -1 means fruit
     while True:
-        fruit_pos = np.random.randint(0, len(board))
+        fruit_pos = (
+                np.random.randint(0, len(board)),
+                np.random.randint(0, len(board))
+                )
         if board[fruit_pos] == 0:
             board[fruit_pos] = -1
+            break
+    return board
 
 def init():
     bdim = 15
-    board = np.zeros(bdim**2)
+    board = np.zeros(bdim**2).reshape((bdim, bdim))
     #Init snake
-    board[int(bdim)/2, int(bdim)/2] = 1
-    board[int(bdim)/2 + 1, int(bdim)/2] = 2
+    board[int(bdim/2), int(bdim/2)] = 1
+    board[int(bdim/2) + 1, int(bdim/2)] = 2
+    return board
+
+if __name__ == '__main__':
+    board = init()
+    board = spawn_fruit(board)
+    print(board)
