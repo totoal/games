d83e205d4b0086ef708b5329a11a7adf3e15be54
totoal <totoal@uv.es>
2021-06-27 18:05:38 +0200

    Using Bold font in sequence. Now displaying the input sequence as the player types

--- simonsays/simonsays.py
+++ simonsays/simonsays.py
@@ -23,6 +23,7 @@ def input_seq(seq_len):
     sequence = []
     for i in range(seq_len):
         sequence.append(getkey())
+        print(' '.join(sequence), end = '\r')
     return sequence
 
 def game(dt, round, seq):
@@ -31,13 +32,13 @@ def game(dt, round, seq):
     time.sleep(dt)
     for event in seq:
         if event == 0:
-            buttons[0] = Fore.RED + 'W' + Style.RESET_ALL
+            buttons[0] = Style.BRIGHT + Fore.RED + 'W' + Style.RESET_ALL
         if event == 1:
-            buttons[1] = Fore.GREEN + 'A' + Style.RESET_ALL
+            buttons[1] = Style.BRIGHT + Fore.GREEN + 'A' + Style.RESET_ALL
         if event == 2:
-            buttons[2] = Fore.YELLOW + 'D' + Style.RESET_ALL
+            buttons[2] = Style.BRIGHT + Fore.YELLOW + 'D' + Style.RESET_ALL
         if event == 3:
-            buttons[3] = Fore.BLUE + 'S' + Style.RESET_ALL
+            buttons[3] = Style.BRIGHT + Fore.BLUE + 'S' + Style.RESET_ALL
         disp_butt(buttons, round)
         time.sleep(dt*2)
         buttons = ['w', 'a', 'd', 's']
@@ -67,11 +68,11 @@ if __name__ == '__main__':
             seq.append(np.random.randint(0,4))
             win = game(0.3, round, seq)
             if win:
-                print(Fore.GREEN + 'Nice!' + Style.RESET_ALL)
+                print(Fore.GREEN + '\nNice!' + Style.RESET_ALL)
                 time.sleep(1.5)
             round += 1
             if not win: break
-        print(Fore.RED + 'You lost :(' + Style.RESET_ALL)
+        print(Fore.RED + '\nYou lost :(' + Style.RESET_ALL)
         ng = '0'
         while ng != 'y' and ng != 'n':
             ng = input('Play again? y/n ')
